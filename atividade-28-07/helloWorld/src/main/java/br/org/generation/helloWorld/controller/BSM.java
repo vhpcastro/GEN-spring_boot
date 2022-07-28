e8ab4c5d2f8c56e3eb0b3058385874e0fb4a4c1f
Victor Hugo Pires <vhpcastro0811@gmail.com>
2022-07-28 20:07:37 -0300

    Criação classe BSMApp + Controller

--- atividade-28-07/helloWorld/src/main/java/br/org/generation/helloWorld/controller/BSM.java
+++ atividade-28-07/helloWorld/src/main/java/br/org/generation/helloWorld/controller/BSM.java
@@ -1,19 +1,39 @@
 package br.org.generation.helloWorld.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/bsm")
-
+@RestController // usado para definir como controlador
+@RequestMapping("/") // usado para definir como local de exibição no localhost
 public class BSM {
 	
-	@GetMapping
-	
-	public String bsm() {
-		return "Mentalidade: Orientação ao futuro, Responsabilidade pessoal, Mentalidade de Crescimento, Persistência."
-				+ " || Habilidades: Trabalho em Equipe, Orientação aos Detalhes, Proatividade, Comunicação.";
+	@GetMapping("/") // usado para pegar as informações abaixo e imprimir no navegador
+	public String raiz() {
+		return "Hello World!"; // return é usado para retornar a informação na tela do usuário
 	}
-
+	@GetMapping("/bsm") 	
+	public String bsm() {
+		return "<ul>"
+				+ "<h2>Mentalidade:</h2> "
+				+ "<li>Orientação ao futuro</li>"
+				+ "<li>Responsabilidade pessoal</li>"
+				+ "<li>Mentalidade de Crescimento</li>"
+				+ "<li>Persistência.</li>"
+				+ "<br>"
+				+ "<h2>Habilidades:</h2>"
+				+ "<li>Trabalho em Equipe</li>"
+				+ "<li>Orientação aos Detalhes</li>"
+				+ "<li>Proatividade</li>"
+				+ "<li>Comunicação</li>"
+			+ "</ul>"; 
+	}
+	
+	@GetMapping("/objetivos")
+	public String obj() {
+		return "Aprendizado em Spring Boot e MySQL.";
+	}
+	
 }
---
+++ atividade-28-07/helloWorld/src/main/java/br/org/generation/helloWorld/controller/BSMApplication.java
@@ -0,0 +1,12 @@
+package br.org.generation.helloWorld.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSMApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BSMApplication.class, args);
+	}
+
+}
